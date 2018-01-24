ef72f8a0f7e402af897be2c403bf4aba18f58cf3
alexZrAl <alexZrAl@users.noreply.github.com>
2018-01-24 00:41:21 -0500

    :)

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String firstStringEver = new String("Test123");
+        System.out.println(firstStringEver);
+    }
+}
