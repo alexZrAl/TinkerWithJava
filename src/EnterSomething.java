f1b527eb56fc1c2222b700f51c9144cba413fbcc
alexZrAl <alexZrAl@users.noreply.github.com>
2018-02-19 03:15:01 -0500

    to kuzmin

--- src/EnterSomething.java
+++ src/EnterSomething.java
@@ -4,6 +4,7 @@ public class EnterSomething
 {
     public static void main(String[] args)
     {
+        System.out.println("Prof Kuzmin, JUST WHAT IS WRONG WITH INTELLIJ AND WHY U HATE IT???");
         System.out.println("CSCI-1010 hw3 refractored in java");
 
         Encrypt nsa = new Encrypt();
